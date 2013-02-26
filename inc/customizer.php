cc23f5cd8e4ae788801e1dad47d87bba0c498813
Eduardo Zulian <eduardozulian@gmail.com>
2013-02-26 00:14:30 -0300

    first commit. thanks, _s

---
+++ inc/customizer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Flutuante Theme Customizer
+ *
+ * @package Flutuante
+ * @since Flutuante 1.2
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ *
+ * @since Flutuante 1.2
+ */
+function flutuante_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'flutuante_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ *
+ * @since Flutuante 1.2
+ */
+function flutuante_customize_preview_js() {
+	wp_enqueue_script( 'flutuante_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20120827', true );
+}
+add_action( 'customize_preview_init', 'flutuante_customize_preview_js' );
